670472f860dcca1da9802e90346937ec6d59bc76
minidonut <nycom13@gmail.com>
2023-05-22 00:34:53 +0900

    chore(frontend): standalone export

--- packages/service-frontend/next.config.js
+++ packages/service-frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: require('path').join(__dirname, '../../'),
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
