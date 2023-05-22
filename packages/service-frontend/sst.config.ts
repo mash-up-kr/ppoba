0d27ecb1b49a057c22356d7acd37a93642eecccf
minidonut <nycom13@gmail.com>
2023-05-22 10:46:44 +0900

    feat: make domain private

---
+++ packages/service-frontend/sst.config.ts
@@ -0,0 +1,26 @@
+import { SSTConfig } from 'sst';
+import { NextjsSite } from 'sst/constructs';
+
+export default {
+  config(_input) {
+    return {
+      name: 'ppoba-frontend',
+      region: 'ap-northeast-2',
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, 'site', {
+        customDomain: {
+          domainName: process.env.DOMAIN_NAME,
+          hostedZone: process.env.AWS_HOST_ZONE,
+        },
+        runtime: 'nodejs18.x',
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
