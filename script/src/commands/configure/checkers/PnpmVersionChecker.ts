a39aa6f218a8a3d6fc71c8f3cdb33deed9d2b185
Lee <43488305+poiu694@users.noreply.github.com>
2023-06-10 16:26:10 +0900

    chore: merge master branch

--- script/src/commands/configure/checkers/PnpmVersionChecker.ts
+++ script/src/commands/configure/checkers/PnpmVersionChecker.ts
@@ -5,10 +5,10 @@ import { createChecker } from '../Chekcer';
 export const PnpmVersionChecker = createChecker(async () => {
   try {
     const versionString = shell.orThrow`pnpm --version`;
-    if (/^7\./.test(versionString)) {
+    if (/^8\./.test(versionString)) {
       return versionString;
     }
-    throw new CheckFailureError(versionString, ['pnpm 버전을 7.X로 맞춰주세요']);
+    throw new CheckFailureError(versionString, ['pnpm 버전을 8.X로 맞춰주세요']);
   } catch (e: any) {
     throw new CheckFailureError(e.message, ['pnpm이 제대로 설치되어있는지 확인해주세요', 'npm -g install pnpm@^7']);
   }
