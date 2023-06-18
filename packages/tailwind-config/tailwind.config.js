3f256eafa5b39a33008510706b32a6330e8a385a
Lee <43488305+poiu694@users.noreply.github.com>
2023-06-18 21:11:59 +0900

    refactor: add slide-left-toggle animation

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -37,10 +37,40 @@ module.exports = {
             left: 'calc(100% - 3px)',
           },
         },
+        'slide-left-toggle': {
+          '0%': {
+            width: '24px',
+            left: 'calc(100% - 3px)',
+          },
+          '20%': {
+            width: '16px',
+            left: 'calc(100% - 3px)',
+            'border-radius': '9px 8px 8px 9px / 12px 12px 12px 12px',
+          },
+          '40%': {
+            width: '35px',
+            left: 'calc(50% + 24px)',
+            'border-radius': '12px 23px 23px 12px / 12px 12px 12px 12px',
+          },
+          '60%': {
+            width: '35px',
+            left: 'calc(50% + 15px)',
+            'border-radius': '23px 12px 12px 23px / 12px 12px 12px 12px',
+          },
+          '80%': {
+            width: '14px',
+            left: 'calc(50% - 10px)',
+            'border-radius': '9px 8px 8px 9px / 12px 12px 12px 12px',
+          },
+          '100%': {
+            width: '24px',
+            left: '50%'
+          },
+        },
       },
       animation: {
-        'slide-right-toggle': 'slide-right-toggle 0.7s linear',
-        'slide-left-toggle': 'slide-right-toggle 0.7s linear reverse'
+        'slide-right-toggle': 'slide-right-toggle 0.4s linear',
+        'slide-left-toggle': 'slide-left-toggle 0.4s linear'
       }
     },
   },
--- packages/ui/src/components/ToggleButton.tsx
+++ packages/ui/src/components/ToggleButton.tsx
@@ -8,16 +8,17 @@ interface Props {
 
 function ToggleButton({ id = 'switch', value, onClick }: Props): JSX.Element {
   return (
-    <button type="button" onClick={onClick} className="w-[54px] h-[30]px cursor-pointer">
+    <button type="button" onClick={onClick}>
       <label
+        id={id}
         htmlFor={id}
         className={`block w-[54px] h-[30px] ${
           value ? 'bg-[#E65C5C]' : 'bg-[#DFDFDF]'
         } relative rounded-[200px] cursor-pointer`}
       >
         <span
-          className={`w-6 h-6 bg-[#ffffff] absolute top-[3px] -translate-x-full rounded-full cursor-pointer transition-all duration-700 ease-in-out ${
-            value ? 'animate-slide-right-toggle left-[calc(100%-3px)]' : 'left-1/2'
+          className={`w-6 h-6 bg-[#ffffff] absolute top-[3px] -translate-x-full rounded-full transition-all ${
+            value ? 'animate-slide-right-toggle left-[calc(100%-3px)]' : 'animate-slide-left-toggle '
           }`}
         />
       </label>
