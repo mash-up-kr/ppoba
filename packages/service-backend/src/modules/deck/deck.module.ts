c690b4c827b23ae2d7d4de5374a52e35e2b2116d
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2023-07-01 17:52:19 +0900

    feat: deck repository module inject

--- packages/service-backend/src/modules/deck/deck.module.ts
+++ packages/service-backend/src/modules/deck/deck.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DeckService } from './deck.service';
 import { DeckController } from './deck.controller';
+import { DeckRepository } from './deck.repository';
 
 @Module({
   controllers: [DeckController],
-  providers: [DeckService]
+  providers: [DeckService, DeckRepository],
 })
 export class DeckModule {}
