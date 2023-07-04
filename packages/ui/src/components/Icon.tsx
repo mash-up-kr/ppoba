57b51c807bd4352d2986e3e7d216a5ffd70b35c0
swimjiy <swimjiy@gmail.com>
2023-07-05 00:12:19 +0900

    Add icon component

---
+++ packages/ui/src/components/Icon.tsx
@@ -0,0 +1,66 @@
+import type { JSX } from 'react';
+
+import { ReactComponent as Add } from '../assets/icons/Add.svg';
+import { ReactComponent as Alert } from '../assets/icons/Alert.svg';
+import { ReactComponent as Arrow } from '../assets/icons/Arrow.svg';
+import { ReactComponent as Back } from '../assets/icons/Back.svg';
+import { ReactComponent as Bookmark } from '../assets/icons/Bookmark.svg';
+import { ReactComponent as Cardcount } from '../assets/icons/Cardcount.svg';
+import { ReactComponent as Close } from '../assets/icons/Close.svg';
+import { ReactComponent as DeckAdd } from '../assets/icons/DeckAdd.svg';
+import { ReactComponent as Go } from '../assets/icons/Go.svg';
+import { ReactComponent as House } from '../assets/icons/House.svg';
+import { ReactComponent as kakao } from '../assets/icons/kakao.svg';
+import { ReactComponent as Order } from '../assets/icons/Order.svg';
+import { ReactComponent as Pen } from '../assets/icons/Pen.svg';
+import { ReactComponent as Question } from '../assets/icons/Question.svg';
+import { ReactComponent as Share } from '../assets/icons/Share.svg';
+import { ReactComponent as Shuffle } from '../assets/icons/Shuffle.svg';
+import { ReactComponent as Top } from '../assets/icons/Top.svg';
+import { ReactComponent as Touch } from '../assets/icons/Touch.svg';
+import { ReactComponent as Trash } from '../assets/icons/Trash.svg';
+import { ReactComponent as Welcome } from '../assets/icons/Welcome.svg';
+
+const ICONS = {
+  add: Add,
+  arrow: Arrow,
+  alert: Alert,
+  back: Back,
+  bookmark: Bookmark,
+  cardcount: Cardcount,
+  close: Close,
+  deckAdd: DeckAdd,
+  go: Go,
+  house: House,
+  kakao: kakao,
+  order: Order,
+  pen: Pen,
+  question: Question,
+  share: Share,
+  shuffle: Shuffle,
+  top: Top,
+  touch: Touch,
+  trash: Trash,
+  welcome: Welcome,
+};
+
+type IconType = keyof typeof ICONS;
+
+type IconProps = {
+  type: IconType;
+  width?: number | string;
+  height?: number | string;
+};
+
+function Icon({ type, width, height }: IconProps): JSX.Element {
+  const SVGIcon = ICONS[type];
+
+  return (
+    <SVGIcon
+      width={typeof width === 'number' ? `${width}px` : width}
+      height={typeof height === 'number' ? `${height}px` : height}
+    />
+  );
+}
+
+export default Icon;
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './Button';
 export { default as ToggleButton } from './ToggleButton';
+export { default as Icon } from './Icon';
