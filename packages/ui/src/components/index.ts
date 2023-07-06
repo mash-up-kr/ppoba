5028f9e86c01844dd6b5c2f20831db25ef277a3c
Lee <43488305+poiu694@users.noreply.github.com>
2023-07-06 23:07:10 +0900

    feat: add AlertMessage

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as AlertMessage } from './AlertMessage';
 export { default as Button } from './Button';
 export { default as Icon } from './Icon';
 export { default as ToggleButton } from './ToggleButton';
