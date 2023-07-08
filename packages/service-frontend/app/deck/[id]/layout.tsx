f77464b790b5cd7bfd4cf572253fec786538d946
Lee <43488305+poiu694@users.noreply.github.com>
2023-07-08 23:49:48 +0900

    feat: add common header component

--- packages/service-frontend/app/deck/[id]/layout.tsx
+++ packages/service-frontend/app/deck/[id]/layout.tsx
@@ -1,7 +1,22 @@
+'use client'
+import { ContentWrapper, Header } from '@/components'
+
 interface Props {
   children: React.ReactNode
 }
 
 export default function Layout({ children }: Props): JSX.Element {
-  return <div>{children}</div>
+  return (
+    <ContentWrapper>
+      <Header
+        title="뉴 매시업 이미지 게임"
+        className="bg-[#FFA63F]"
+        leftIconType="back"
+        rightIconType="share"
+        onClickLeftIcon={() => console.log('left')}
+        onClickRightIcon={() => console.log('right')}
+      />
+      {children}
+    </ContentWrapper>
+  )
 }
