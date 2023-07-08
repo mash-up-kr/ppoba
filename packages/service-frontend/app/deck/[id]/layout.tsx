e1f9f13a5e9dacd99ab50a47c0d5c82f15ce82f4
Lee <43488305+poiu694@users.noreply.github.com>
2023-07-09 00:16:27 +0900

    feat: add loginHeader component

--- packages/service-frontend/app/deck/[id]/layout.tsx
+++ packages/service-frontend/app/deck/[id]/layout.tsx
@@ -8,14 +8,7 @@ interface Props {
 export default function Layout({ children }: Props): JSX.Element {
   return (
     <ContentWrapper>
-      <Header
-        title="뉴 매시업 이미지 게임"
-        className="bg-[#FFA63F]"
-        leftIconType="back"
-        rightIconType="share"
-        onClickLeftIcon={() => console.log('left')}
-        onClickRightIcon={() => console.log('right')}
-      />
+      <Header title="로그인" className="justify-end" />
       {children}
     </ContentWrapper>
   )
