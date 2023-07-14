8a722db273125b418b39394cb0214c09fbef124a
Jiwon Park <43839859+pjw5521@users.noreply.github.com>
2023-07-14 18:19:32 +0900

    feat: add card update db logic

--- packages/service-backend/src/modules/card/CardRepository.ts
+++ packages/service-backend/src/modules/card/CardRepository.ts
@@ -33,22 +33,12 @@ export class CardRepository {
       return deletedCard;
     }
     
-    async save(id: string, card: Card) {
-      
-      const cardItem = await this.cardModel.findByIdAndUpdate(
-        id,
-        {
-          content: card.content, // 업데이트할 필드 추가
-          deletedAt: card.deletedAt, // 업데이트할 필드 추가
-        },
-        { new: true } // 업데이트 후의 카드 객체 반환 설정
+    async update(id: string, card: Card): Promise<Object> {
+      const updateCard = await this.cardModel.updateOne(
+        { id: id },
+        { $set: { content: card.content } }
       );
-    
-      if (!cardItem) {
-        throw new NotFoundException('카드를 찾을 수 없습니다.');
-      }
-    
-      console.log(cardItem.toJSON());
-      return assert<Card>(cardItem.toJSON());
+
+      return updateCard;
     }
 }
