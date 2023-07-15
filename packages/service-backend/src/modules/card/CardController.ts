feba1c7da211ea7d1d012b3b7087e59f4d6a1e81
Anjonghun <dkswhdgnsvv@gmail.com>
2023-07-15 18:15:19 +0900

    refactor: create card api modify request body (#54)
    refactor: create card requst body deck-id

--- packages/service-backend/src/modules/card/CardController.ts
+++ packages/service-backend/src/modules/card/CardController.ts
@@ -1,25 +1,30 @@
 import { Controller, Post, Body, Get, Param, Delete, Patch } from '@nestjs/common';
 import { CardService } from './CardService';
 import { Card } from '../../core/database';
-
+import { ApiTags } from '@nestjs/swagger';
+import { createCardDto } from './dto/createCardDto';
 @Controller('cards')
 export class CardController {
   constructor(private readonly cardService: CardService) {}
 
+  @ApiTags('card')
   @Post()
   async CreateCard(
-    @Body('cardList') cardDto: Omit<Card, 'createdAt' | 'updatedAt' | 'deletedAt'>[]
+    @Body('cardList') cardDto: createCardDto[],
+    @Body('deckId') deckId: string
   ): Promise<any> {
-    const result = await this.cardService.create(cardDto);
+    const result = await this.cardService.create(cardDto, deckId);
     return { result: result };
   }
 
+  @ApiTags('card')
   @Delete(':id')
   async deleteCard(@Param('id') id: string): Promise<any> {
     await this.cardService.deleteCard(id);
     return { message: '카드가 삭제되었습니다.' };
   }
 
+  @ApiTags('card')
   @Patch(':id')
   async updateCard(
     @Param('id') id: string,
