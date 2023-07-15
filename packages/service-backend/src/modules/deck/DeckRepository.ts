516bee4f91974814f18d1b6d7fe9fc71fc0e274d
Jonghun An <jonghun.an@bucketstore.com>
2023-07-15 14:30:43 +0900

    feat: get deck row by id

--- packages/service-backend/src/modules/deck/DeckRepository.ts
+++ packages/service-backend/src/modules/deck/DeckRepository.ts
@@ -1,27 +1,36 @@
-import {Injectable, InternalServerErrorException} from '@nestjs/common';
-import {InjectModel} from "../../core/database";
-import { SoftDeleteModel } from 'soft-delete-plugin-mongoose'
-import {DeckDocument} from "../../core/database/Deck";
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '../../core/database';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { DeckDocument } from '../../core/database/Deck';
 
 @Injectable()
 export class DeckRepository {
-  constructor(@InjectModel.Deck private readonly deckModel: SoftDeleteModel<DeckDocument>) {
-  }
+  constructor(@InjectModel.Deck private readonly deckModel: SoftDeleteModel<DeckDocument>) {}
 
-  async create(name: string, userId:string): Promise<string> {
-  try{
+  async create(name: string, userId: string): Promise<string> {
+    try {
       const deck = await this.deckModel.create({
         name,
         userId,
-    })
-    return deck.id;
-  }catch(error){
-    throw new InternalServerErrorException(`error: ${error}`);
-  }
+      });
+      return deck.id;
+    } catch (error) {
+      throw new InternalServerErrorException(`error: ${error}`);
+    }
   }
 
-  async findOne(id: string) {
-    return `This action returns a #${id} deck`;
+  async findOne(id: string): Promise<any> {
+    try {
+      const deck = await this.deckModel.findOne({ id });
+
+      if (!deck) {
+        throw new NotFoundException(`Deck with id ${id} not found`);
+      }
+
+      return deck;
+    } catch (error) {
+      throw new InternalServerErrorException(`error: ${error}`);
+    }
   }
 
   async update(id: string) {
