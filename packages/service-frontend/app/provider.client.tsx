936dc3f24f68afbb2c512a9112c13357337b1732
Yung-Ju Kim <yungjurick@gmail.com>
2023-07-15 22:44:46 +0900

    feat: add chkara ui

--- packages/service-frontend/app/provider.client.tsx
+++ packages/service-frontend/app/provider.client.tsx
@@ -2,12 +2,22 @@
 import type { PropsWithChildren, JSX } from 'react'
 import { useState } from 'react'
 
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { theme } from '@/theme'
 
 function Provider({ children }: PropsWithChildren): JSX.Element {
   const [client] = useState(() => new QueryClient())
 
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={client}>
+      <CacheProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </CacheProvider>
+    </QueryClientProvider>
+  )
 }
 
 export default Provider
