8ec92f760d10f44f3bdae0d93d85b34adb18a188
Lee <43488305+poiu694@users.noreply.github.com>
2023-07-15 16:51:27 +0900

    feat: 덱 생성 완료 페이지 (#31)
    * feat: add Home icon
    
    * feat: add deck-create-complte page

---
+++ packages/service-frontend/app/complete/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Lottie from 'lottie-react'
+import { Button } from '@ppoba/ui'
+
+import { Header } from '@/components'
+import addDeckLottie from '@/public/lottie/addDeckLottie.json'
+
+export default function DeckCreateCompletePage(): JSX.Element {
+  return (
+    <div className="min-h-screen px-[16px] flex flex-col gap-[20px]">
+      <Header rightIconType="home" />
+      <h1 className="text-black headline-1 whitespace-pre pt-[72px]">
+        {'축하해!\n게임을 완성했어'}
+      </h1>
+
+      <Lottie animationData={addDeckLottie} />
+
+      <Button
+        size="large"
+        className="headline-4 text-white"
+        rightIcon="goLight"
+      >
+        플레이하기
+      </Button>
+    </div>
+  )
+}
