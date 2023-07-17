a39b64bbb7deb135d9f774db31d7e255c411d89a
Yung Ju Rick Kim <yungjurick@gmail.com>
2023-07-17 09:41:28 +0900

    feat: add chkara ui (#59)

---
+++ packages/service-frontend/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  fonts: {
+    heading: 'var(--font-pretendard)',
+    body: 'var(--font-pretendard)',
+  },
+})
