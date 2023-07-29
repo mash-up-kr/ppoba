4f223b4758d62aa1a9dd17b2714b4b001f9d4937
Lee <43488305+poiu694@users.noreply.github.com>
2023-07-29 20:10:59 +0900

    chore: rename taro => tarot

--- packages/service-frontend/app/deck/[id]/tarot/page.tsx
+++ packages/service-frontend/app/deck/[id]/tarot/page.tsx
@@ -160,6 +160,7 @@ export default function TaroPlayPage(): JSX.Element {
                 onClickNextCard={handleClickNextCard}
                 onClickCurrentCard={handleClickCurrentCard}
               />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
             </motion.div>
           ) : (
             <motion.div
