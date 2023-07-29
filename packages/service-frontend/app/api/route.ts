cec06505aca8bf5f5950cf308118080b7da1bfdf
Lee <43488305+poiu694@users.noreply.github.com>
2023-07-29 14:54:57 +0900

    chore: rollback sentry web

--- packages/service-frontend/app/api/route.ts
+++
@@ -1,5 +0,0 @@
-// A faulty API route to test Sentry's error monitoring
-export default function handler(req: Request, res: Response): number {
-  throw new Error('Sentry Example API Route Error')
-  return 1
-}
