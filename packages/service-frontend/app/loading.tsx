68e2b8ae9068711e59c427d1fc8e63ba5123c6ec
Jiyoung Lim <swimjiy@gmail.com>
2023-08-06 16:38:57 +0900

    feat: implement api call on main api, add 404 and loading page (#115)
    * Add loading page
    
    * Add not-found page
    
    * Apply game deck api

---
+++ packages/service-frontend/app/loading.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Lottie from 'lottie-react'
+
+import { ContentWrapper } from '@/app/components'
+import loginLottie from '@/public/lottie/loginLottie.json'
+
+// TODO: Change loading animation
+export default function Loading(): JSX.Element {
+  return (
+    <ContentWrapper>
+      <div className='min-h-screen px-[16px] flex flex-col gap-[20px] justify-center items-center'>
+        Loading...
+        <Lottie animationData={loginLottie} />
+      </div>
+    </ContentWrapper>
+  )
+}
