df0570276559e151e04ba19ce9b446751cbeb44b
Lee <43488305+poiu694@users.noreply.github.com>
2023-08-07 23:51:27 +0900

    feat: squircle lib (#120)
    * feat: squircle lib
    
    * chore: add eol

--- packages/ui/src/components/SecondaryButton.tsx
+++ packages/ui/src/components/SecondaryButton.tsx
@@ -1,5 +1,7 @@
 import type { JSX, PropsWithChildren } from 'react';
 
+import { Squircle } from 'corner-smoothing';
+
 import { IconType } from './Icon';
 import { Icon } from '../components';
 
@@ -8,7 +10,8 @@ type ButtonSize = 'small' | 'medium';
 type Props = {
   size?: ButtonSize;
   rightIcon?: IconType;
-} & React.ComponentPropsWithoutRef<'button'>;
+  disabled?: boolean;
+} & React.ComponentPropsWithoutRef<'div'>;
 
 const sizes = {
   small: 'w-[100px]',
@@ -26,12 +29,18 @@ function SecondaryButton({
   const buttonSize = sizes[size ?? 'small'];
 
   return (
-    <button
-      type="button"
-      className={`flex justify-center items-center ${buttonSize} h-[60px] text-base text-grey-800 font-bold leading-[150%] tracking-[-0.16px] border border-grey-600 rounded-[20px] bg-white
-        hover:text-grey-600 hover:border-grey-400 hover:bg-[#EFEFEF]
-        disabled:text-grey-300 disabled:border-grey-200 disabled:bg-white 
-        ${disabled ? 'disabled:cursor-not-allowed' : 'cursor-pointer'}
+    <Squircle
+      role="button"
+      cornerRadius={24}
+      borderWidth={1}
+      className={`relative flex justify-center items-center ${buttonSize} h-[60px] text-base text-grey-800 font-bold leading-[150%] tracking-[-0.16px] transition-all bg-grey-600
+        text-grey-800
+        hover:bg-grey-400 hover:text-grey-600
+        disabled:bg-grey-200 disabled:text-grey-300
+        before:absolute before:w-full before:h-full before:bg-white before:transition-all
+        before:disabled:bg-white 
+        before:hover:bg-[#EFEFEF]
+        ${disabled ? 'before:cursor-not-allowed cursor-not-allowed' : 'cursor-pointer'}
         ${className}`}
       {...props}
     >
@@ -44,7 +53,7 @@ function SecondaryButton({
           className={`inline-block align-bottom ml-[2px] ${disabled && 'opacity-40 cursor-not-allowed'}`}
         />
       )}
-    </button>
+    </Squircle>
   );
 }
 
