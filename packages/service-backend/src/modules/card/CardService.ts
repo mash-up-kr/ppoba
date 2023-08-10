6a59f4acaee863b81f740724aef83e55598760ab
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2023-08-11 00:10:27 +0900

    refactor: update deck totalCardCount column at create card api

--- packages/service-backend/src/modules/card/CardService.ts
+++ packages/service-backend/src/modules/card/CardService.ts
@@ -2,13 +2,19 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Card } from '../../core/database';
 import { CardRepository } from './CardRepository';
 import { CardList } from './dto/createCardDto';
+import { DeckRepository } from '../deck/DeckRepository';
 
 @Injectable()
 export class CardService {
-  constructor(private readonly cardRepository: CardRepository) {}
+  constructor(
+    private readonly cardRepository: CardRepository,
+    private readonly deckRepository: DeckRepository
+  ) {}
 
   async create(cardList: CardList[], deckId: string): Promise<boolean> {
-    return await this.cardRepository.create(cardList, deckId);
+    const totalCardCount = cardList.length;
+    await this.cardRepository.create(cardList, deckId);
+    return await this.deckRepository.updateDeckTotalCardCount(deckId, totalCardCount);
   }
 
   async deleteCard(id: string): Promise<boolean> {
