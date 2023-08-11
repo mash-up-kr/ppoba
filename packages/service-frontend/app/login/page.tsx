759177592d9439eb0207770a247718486df7efaa
swimjiy <swimjiy@gmail.com>
2023-08-12 04:53:59 +0900

    Navigate to the previous route to login user

--- packages/service-frontend/app/login/page.tsx
+++ packages/service-frontend/app/login/page.tsx
@@ -1,14 +1,22 @@
 'use client'
 
 import Lottie from 'lottie-react'
+import { useRouter } from 'next/navigation'
 import { KakaoButton } from '@ppoba/ui'
 
+import useIsLoggedIn from '@/hooks/useIsLoggedIn'
 import loginLottie from '@/public/lottie/loginLottie.json'
 
 import { useLogin } from './hooks'
 
 export default function Login(): JSX.Element {
   const { handleLoginClick } = useLogin()
+  const isLoggedIn = useIsLoggedIn()
+  const router = useRouter()
+
+  if (isLoggedIn) {
+    router.back()
+  }
 
   return (
     <div className="min-h-screen px-[16px] py-[80px] flex flex-col justify-between">
